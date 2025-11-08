166d4706196f406fab6b8766066979dfb3c386b3
Donna <donna.zavalaperez@gmail.com>
2025-11-08 09:56:15 +0000

    GoogleAuthError: [VertexAI.GoogleAuthError]: Unable to authenticate you

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,11 +1,18 @@
 import {genkit} from 'genkit';
 import {vertexAI} from '@genkit-ai/vertexai';
+import * as sa from '../../sa.json';
+
+const serviceAccount = {
+  client_email: sa.client_email,
+  private_key: sa.private_key,
+};
 
 export const ai = genkit({
   plugins: [
     vertexAI({
       projectId: 'studio-99491860-5533f',
       location: 'us-central1',
+      serviceAccount,
     }),
   ],
   model: 'vertexai/gemini-1.5-flash-001',
