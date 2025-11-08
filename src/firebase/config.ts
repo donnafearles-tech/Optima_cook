1b4cc57f0091f11821df619e68cfac15f56b7a67
Donna <donna.zavalaperez@gmail.com>
2025-11-08 09:59:15 +0000

    Sustituye la api de vertex por esta : AQ.Ab8RN6LabaYV_Au0M9UosXufnAOGoD5

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-99491860-5533f",
   "appId": "1:941568288177:web:7dcd9a4d3cf17c715b0990",
-  "apiKey": "AIzaSyBFD6y9l0oUCc41xaMmL3__n3-zYFjHf8w",
+  "apiKey": "AQ.Ab8RN6LabaYV_Au0M9UosXufnAOGoD5qnbJKSHQVKvPaRDwYeA",
   "authDomain": "studio-99491860-5533f.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "941568288177"
