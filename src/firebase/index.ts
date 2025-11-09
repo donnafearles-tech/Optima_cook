7d0618cf07ac3db40de48873ebec757bfff68e8d
Donna Zavala <donna.fearles@gmail.com>
2025-11-09 09:31:00 +0000

    Me dice te recuested action is invalid : https://studio-99491860-5533f.f

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,9 +1,19 @@
 'use client';
 
-import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { Auth, getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore'
+
+// Moviendo la configuración aquí para centralizarla
+export const firebaseConfig = {
+  "projectId": "studio-99491860-5533f",
+  "appId": "1:941568288177:web:7dcd9a4d3cf17c715b0990",
+  "apiKey": "AQ.Ab8RN6LabaYV_Au0M9UosXufnAOGoD5qnbJKSHQVKvPaRDwYeA",
+  "authDomain": "studio-99491860-5533f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "941568288177"
+};
+
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
