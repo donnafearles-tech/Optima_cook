f3f4297546179c4df6f31ded64e6c756a55daeb0
Donna <donna.zavalaperez@gmail.com>
2025-11-27 03:32:59 +0000

    puedo  instalar en esta  cli  la  bubblewrap para crear aplicaciones pwa

---
+++ src/next.config.ts
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public'
+});
+
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+};
+
+module.exports = withPWA(nextConfig);
